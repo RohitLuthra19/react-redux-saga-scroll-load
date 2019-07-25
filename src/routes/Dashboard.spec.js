91046313791142ce3075b602fa286d5337ca03e6
Rohit Kumar <rohit.kumar@mpulsemobile.com>
2019-07-25 17:59:56 +0530

    corrected test

--- src/routes/Dashboard.spec.js
+++ src/routes/Dashboard.spec.js
@@ -3,13 +3,15 @@ import Dashboard from './Dashboard';
 import '../setupTests';
 import renderer from 'react-test-renderer';
 import store from '../redux/store';
+import { Provider } from "react-redux";
 
-describe('Dashboard renders',() => {
-
-  it('Dashboard component created', () => {
+describe("Dashboard Component", () => {
+  it("should render without throwing an error", () => {
     const rendered = renderer.create(
-      <Dashboard store={store} />
-    );
+      <Provider store={store}>
+          <Dashboard />
+      </Provider>
+    )
     expect(rendered.toJSON()).toMatchSnapshot();
-  })
-})
+  });
+});
